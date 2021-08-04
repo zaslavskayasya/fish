d9c396164bd99500dd533b926c75f284294b2d9b
Sommelier <Arty.03.07@gmail.com>
2021-08-05 00:10:32 +0300

    Добавлена линия

--- js/live-line.js
+++ js/live-line.js
@@ -1,10 +1,24 @@
-let {offsetTop, clientHeight} = wrapper = document.body.querySelector('.wrapper'),
-	extraOffset = 80,
-	moving = function() {
-		wrapper.style.height = clientHeight - this.scrollY - extraOffset;
+let point = document.createElement('span');
+point.dataset.wrapper = 'end';
+document.body.appendChild(point);
+
+let {clientHeight} = wrapper = document.querySelector('.wrapper'),
+	startPoint = document.body.querySelector('.what_fish-section').offsetTop,
+	endPoint = document.body.querySelector('[data-wrapper="end"]').offsetTop;
+
+let moving = function(){
+		if (scrollY >= 1385) {
+			wrapper.style.display = 'block';
+		} else {
+			wrapper.style.display = 'none';
+		}
+		
+		if (scrollY >= startPoint){
+			wrapper.style.height = clientHeight - scrollY + 'px';
+		}
 	};
 
-window.addEventListener('scroll', function(e){
+window.addEventListener('scroll', function(){
 	moving();
 }, false);
 
